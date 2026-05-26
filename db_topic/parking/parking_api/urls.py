6895fea82c4706cc02ecbdcf09d140034c5fbf13
ariel363454 <kirito1070100@gmail.com>
2026-05-27 01:28:28 +0800

    Update urls.py

--- db_topic/parking/parking_api/urls.py
+++ db_topic/parking/parking_api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/parking-bounds/', views.get_parking_bounds, name='parking_bounds'),
+    path('api/parking＿bounds/', views.get_parking_bounds, name='parking_bounds'),
 ]
